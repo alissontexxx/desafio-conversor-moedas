d50b87cc474bb6de11694f1251c4610ac1211064
alissontexxx <alisson.fonseca@live.com>
2025-10-15 18:37:04 -0300

    projeto iniciado

---
+++ src/main/java/com/alisson/desafio/Main.java
@@ -0,0 +1,11 @@
+package com.alisson.desafio;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Sistema de conversor de moedas!");
+    }
+
+    static void menu() {
+        System.out.println("Escolha uma das opções abaixo:");
+    }
+}
