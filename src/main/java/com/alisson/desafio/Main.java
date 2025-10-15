584c76f8b03abe05c475f6318b1847d561d7294f
alissontexxx <alisson.fonseca@live.com>
2025-10-15 18:47:20 -0300

    criado o menu

--- src/main/java/com/alisson/desafio/Main.java
+++ src/main/java/com/alisson/desafio/Main.java
@@ -1,11 +1,37 @@
 package com.alisson.desafio;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Sistema de conversor de moedas!");
+
+        Scanner sc = new Scanner(System.in);
+
+        int opcao = 0;
+        double valor = 0;
+
+        while (opcao != 7) {
+            menu();
+            opcao = sc.nextInt();
+        }
+
+        sc.close();
+
     }
 
     static void menu() {
         System.out.println("Escolha uma das opções abaixo:");
+        System.out.println("1 - Dólar => Peso Argentino");
+        System.out.println("2 - Peso Argentino => Dólar");
+        System.out.println("3 - Dólar => Real Brasileiro");
+        System.out.println("4 - Real Brasileiro => Dólar");
+        System.out.println("5 - Dólar => Peso Colombiano");
+        System.out.println("6 - Peso Colombiano => Dólar");
+        System.out.println("7 - Sair!");
+        System.out.println();
+        System.out.print("Escolha uma opção válida!\n****************************\n");
+        System.out.print("Opcão: ");
+
     }
 }
